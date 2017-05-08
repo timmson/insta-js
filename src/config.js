97799d1052417525715a041ce765c1803687e066
timmson <timmson666@mail.ru>
2017-05-09 01:24:30 +0300

    #3

---
+++ src/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    user: 'username',
+    userId : 'userid(number)',
+    password: 'password',
+    device: 'yourdevicename',
+    tmpDir: 'tmp',
+    promotion: [
+        {date: '15 10 * * *', tag: 'test'}
+    ],
+    demotion: {date: '15 10 * * *'},
+    limit: 5,
+    delay: 5000
+};
