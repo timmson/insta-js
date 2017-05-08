b67d082c5aedd9658f80ba57e8baa0b1f130882d
timmson <timmson666@mail.ru>
2017-05-09 00:21:41 +0300

    #1

--- src/service.js
+++ src/service.js
@@ -1,13 +1,15 @@
-//const config = require('./config');
+const config = require('./config');
 const log = require('log4js').getLogger();
+const sleep = require('system-sleep');
 const CronJob = require('cron').CronJob;
 const Client = require('instagram-private-api').V1;
 
-//const device = new Client.Device('someuser');
-//const storage = new Client.CookieFileStorage('./' + config.instagram.tmpDir + '/storage.json');
+const device = new Client.Device(config.device);
+const storage = new Client.CookieFileStorage('./' + config.tmpDir + '/storage.json');
 
-//getMedia();
-//new CronJob({cronTime: config.instagram.cron, onTick: getMedia, start: true});
+
+//Demotion
+new CronJob({cronTime: config.demotion.date, onTick: demotion, start: true});
 
 log.info('Service for Instagram API has started');
 log.info('Please press [CTRL + C] to stop');
@@ -23,6 +25,25 @@ process.on('SIGTERM', () => {
 });
 
 
-function getMedia() {
+/*function promotion() {
+    Client.Session.create(device, storage, config.user, config.password);
+}*/
 
+function demotion() {
+    Client.Session.create(device, storage, config.user, config.password)
+        .then(session => {
+            return [session, new Client.Feed.AccountFollowing(session, config.userId, 10).get()]
+        })
+        .spread((session, accounts) =>
+            accounts.slice(0, config.demotion.limit).forEach(account => {
+                Client.Relationship.get(session, account._params.id)
+                    .then(relationship => {
+                        if (!relationship._params.followedBy) {
+                            log.info('Unfollow ' + account._params.username);
+                            Client.Relationship.destroy(session, account._params.id);
+                            sleep(config.delay);
+                        }
+                    });
+            })
+        );
 }
